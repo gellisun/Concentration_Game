179a50a7ab9f90d99270f2b3c6820e53d68ced44
gellisun <uengyaf@hotmail.it>
2023-06-26 18:21:02 +0400

    add flipping feature

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // Constants
 const COLORS = ['aliceblue', 'aliceblue', 'antiquewhite', 'antiquewhite', 'cadetblue', 'cadetblue', 'coral', 'coral', 'cornflowerblue', 'cornflowerblue', 'cornsilk', 'cornsilk', 'darkcyan', 'darkcyan', 'darksalmon', 'darksalmon', 'darkseagreen', 'darkseagreen', 'darkslategray', 'darkslategray'];
 const hiddenColors = COLORS.sort(() => 0.5 - Math.random());
-console.log(hiddenColors)
 
 // State of the game
 let gameHasStarted;
 let cardsFlipped;
-let pickedColor;
 let timer;
 // let timeLeft;
 let winner;
@@ -14,7 +12,6 @@ let winner;
 
 // Cached elements
 const boardEl = document.getElementById('board');
-const cardEl = document.querySelectorAll('card-back'); console.log(cardEl)
 const btnEl = document.getElementById('button');
 const timerEl = document.getElementById('clock');
 const msgEl = document.getElementById('message');
@@ -103,37 +100,30 @@ function handleBtnClick(e) {
 
 function handleMove(e) {
     //guards
-    if (!e.target.classList.contains('card-back')) {
+    if (!e.target.classList.contains('card')) {
         return;
     };
     if (!gameHasStarted) {
         return;
-    }
+    };
     //game has started
     let target = e.target;
-    cardsFlipped = boardEl.querySelectorAll('.card-back.active').length;
-    if (cardsFlipped < 2) {
-        // target.classList.add('active');
-        // hiddenColors.forEach(function(color){
-        //     target.style.backgroundColor = color;
-        // });
-        if (cardsFlipped === 2) return;
+    target.classList.add('active');
+    let colorIndex = target.id;
+    cardsFlipped = boardEl.querySelectorAll('.card.active').length;
+    if (cardsFlipped < 3) {
+        target.style.backgroundColor = hiddenColors[colorIndex];
+     }
+     //hide the div after two cards colors are the same: target.style.visibility = 'hidden';
         //     setTimeout(() => {
         //         target.style.backgroundColor = 'hidden';
         //     }, 2000);
         render();
     }
-}
 
 // Check the game status:
 // While the timer is not 0/or the turns are not maxed out and the are not 10 pairs of cards with active class, keep letting the player click on boxes
-// Else, well done, the game is finished!
-function checkStatus() {
-    while (timer !== 0 && cardEl.className !== 'active') {
-        gameHasStarted = true;
-        winner = null;
-    }
-}
+// Else, well done, the game is finished
 
 
 // Restart
