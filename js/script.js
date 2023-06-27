24aa8c87434a9ebc7efd554f13bb40c315eeee82
gellisun <uengyaf@hotmail.it>
2023-06-27 19:16:18 +0400

    functioning MVP with cards

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
 // // Constants
 const CARDS = [
-    {path: '../imgs/cards/clubs/clubs-A.svg', id: 'A'}, 
-    {path: '../imgs/cards/clubs/clubs-K.svg', id: 'K'},
-    {path: '../imgs/cards/clubs/clubs-Q.svg', id: 'Q'},
-    {path: '../imgs/cards/clubs/clubs-J.svg', id: 'J'},
-    {path: '../imgs/cards/clubs/clubs-r10.svg', id: '10'},
-    {path: '../imgs/cards/clubs/clubs-r09.svg', id: '9'},
-    {path: '../imgs/cards/clubs/clubs-r08.svg', id: '8'},
-    {path: '../imgs/cards/clubs/clubs-r07.svg', id: '7'},
-    {path: '../imgs/cards/clubs/clubs-r06.svg', id: '6'},
-    {path: '../imgs/cards/clubs/clubs-r05.svg', id: '5'},
-    {path: '../imgs/cards/clubs/clubs-r04.svg', id: '4'},
-    {path: '../imgs/cards/clubs/clubs-r03.svg', id: '3'},
-    {path: '../imgs/cards/clubs/clubs-r02.svg', id: '2'},
-    {path: '../imgs/cards/diamonds/diamonds-A.svg', id: 'A'},
-    {path: '../imgs/cards/diamonds/diamonds-K.svg', id: 'K'},
-    {path: '../imgs/cards/diamonds/diamonds-Q.svg', id: 'Q'},
-    {path: '../imgs/cards/diamonds/diamonds-J.svg', id: 'J'},
-    {path: '../imgs/cards/diamonds/diamonds-r10.svg', id: '10'},
-    {path: '../imgs/cards/diamonds/diamonds-r09.svg', id: '9'},
-    {path: '../imgs/cards/diamonds/diamonds-r08.svg', id: '8'},
-    {path: '../imgs/cards/diamonds/diamonds-r07.svg', id: '7'},
-    {path: '../imgs/cards/diamonds/diamonds-r06.svg', id: '6'},
-    {path: '../imgs/cards/diamonds/diamonds-r05.svg', id: '5'},
-    {path: '../imgs/cards/diamonds/diamonds-r04.svg', id: '4'},
-    {path: '../imgs/cards/diamonds/diamonds-r03.svg', id: '3'},
-    {path: '../imgs/cards/diamonds/diamonds-r02.svg', id: '2'},
-    {path: '../imgs/cards/hearts/hearts-A.svg', id: 'A'},
-    {path: '../imgs/cards/hearts/hearts-K.svg', id: 'K'},
-    {path:'../imgs/cards/hearts/hearts-Q.svg', id: 'Q'},
-    {path: '../imgs/cards/hearts/hearts-J.svg', id: 'J'},
-    {path: '../imgs/cards/hearts/hearts-r10.svg', id: '10'},
-    {path: '../imgs/cards/hearts/hearts-r09.svg', id: '9'},
-    {path: '../imgs/cards/hearts/hearts-r08.svg', id: '8'},
-    {path: '../imgs/cards/hearts/hearts-r07.svg', id: '7'},
-    {path: '../imgs/cards/hearts/hearts-r06.svg', id: '6'},
-    {path: '../imgs/cards/hearts/hearts-r05.svg', id: '5'},
-    {path: '../imgs/cards/hearts/hearts-r04.svg', id: '4'},
-    {path: '../imgs/cards/hearts/hearts-r03.svg', id: '3'},
-    {path: '../imgs/cards/hearts/hearts-r02.svg', id: '2'},
-    {path: '../imgs/cards/spades/spades-A.svg', id: 'A'},
-    {path: '../imgs/cards/spades/spades-K.svg', id: 'K'},
-    {path: '../imgs/cards/spades/spades-Q.svg', id: 'Q'},
-    {path: '../imgs/cards/spades/spades-J.svg', id: 'J'},
-    {path: '../imgs/cards/spades/spades-r10.svg', id: '10'},
-    {path: '../imgs/cards/spades/spades-r09.svg', id: '9'},
-    {path: '../imgs/cards/spades/spades-r08.svg', id: '8'},
-    {path: '../imgs/cards/spades/spades-r07.svg', id: '7'},
-    {path: '../imgs/cards/spades/spades-r06.svg', id: '6'},
-    {path: '../imgs/cards/spades/spades-r05.svg', id: '5'},
-    {path: '../imgs/cards/spades/spades-r04.svg', id: '4'},
-    {path: '../imgs/cards/spades/spades-r03.svg', id: '3'},
-    {path: '../imgs/cards/spades/spades-r02.svg', id: '2'}];
+    { path: '../imgs/cards/clubs/clubs-A.svg', id: 'A' },
+    { path: '../imgs/cards/clubs/clubs-K.svg', id: 'K' },
+    { path: '../imgs/cards/clubs/clubs-Q.svg', id: 'Q' },
+    { path: '../imgs/cards/clubs/clubs-J.svg', id: 'J' },
+    { path: '../imgs/cards/clubs/clubs-r10.svg', id: '10' },
+    { path: '../imgs/cards/clubs/clubs-r09.svg', id: '9' },
+    { path: '../imgs/cards/clubs/clubs-r08.svg', id: '8' },
+    { path: '../imgs/cards/clubs/clubs-r07.svg', id: '7' },
+    { path: '../imgs/cards/clubs/clubs-r06.svg', id: '6' },
+    { path: '../imgs/cards/clubs/clubs-r05.svg', id: '5' },
+    { path: '../imgs/cards/clubs/clubs-r04.svg', id: '4' },
+    { path: '../imgs/cards/clubs/clubs-r03.svg', id: '3' },
+    { path: '../imgs/cards/clubs/clubs-r02.svg', id: '2' },
+    { path: '../imgs/cards/diamonds/diamonds-A.svg', id: 'A' },
+    { path: '../imgs/cards/diamonds/diamonds-K.svg', id: 'K' },
+    { path: '../imgs/cards/diamonds/diamonds-Q.svg', id: 'Q' },
+    { path: '../imgs/cards/diamonds/diamonds-J.svg', id: 'J' },
+    { path: '../imgs/cards/diamonds/diamonds-r10.svg', id: '10' },
+    { path: '../imgs/cards/diamonds/diamonds-r09.svg', id: '9' },
+    { path: '../imgs/cards/diamonds/diamonds-r08.svg', id: '8' },
+    { path: '../imgs/cards/diamonds/diamonds-r07.svg', id: '7' },
+    { path: '../imgs/cards/diamonds/diamonds-r06.svg', id: '6' },
+    { path: '../imgs/cards/diamonds/diamonds-r05.svg', id: '5' },
+    { path: '../imgs/cards/diamonds/diamonds-r04.svg', id: '4' },
+    { path: '../imgs/cards/diamonds/diamonds-r03.svg', id: '3' },
+    { path: '../imgs/cards/diamonds/diamonds-r02.svg', id: '2' },
+    { path: '../imgs/cards/hearts/hearts-A.svg', id: 'A' },
+    { path: '../imgs/cards/hearts/hearts-K.svg', id: 'K' },
+    { path: '../imgs/cards/hearts/hearts-Q.svg', id: 'Q' },
+    { path: '../imgs/cards/hearts/hearts-J.svg', id: 'J' },
+    { path: '../imgs/cards/hearts/hearts-r10.svg', id: '10' },
+    { path: '../imgs/cards/hearts/hearts-r09.svg', id: '9' },
+    { path: '../imgs/cards/hearts/hearts-r08.svg', id: '8' },
+    { path: '../imgs/cards/hearts/hearts-r07.svg', id: '7' },
+    { path: '../imgs/cards/hearts/hearts-r06.svg', id: '6' },
+    { path: '../imgs/cards/hearts/hearts-r05.svg', id: '5' },
+    { path: '../imgs/cards/hearts/hearts-r04.svg', id: '4' },
+    { path: '../imgs/cards/hearts/hearts-r03.svg', id: '3' },
+    { path: '../imgs/cards/hearts/hearts-r02.svg', id: '2' },
+    { path: '../imgs/cards/spades/spades-A.svg', id: 'A' },
+    { path: '../imgs/cards/spades/spades-K.svg', id: 'K' },
+    { path: '../imgs/cards/spades/spades-Q.svg', id: 'Q' },
+    { path: '../imgs/cards/spades/spades-J.svg', id: 'J' },
+    { path: '../imgs/cards/spades/spades-r10.svg', id: '10' },
+    { path: '../imgs/cards/spades/spades-r09.svg', id: '9' },
+    { path: '../imgs/cards/spades/spades-r08.svg', id: '8' },
+    { path: '../imgs/cards/spades/spades-r07.svg', id: '7' },
+    { path: '../imgs/cards/spades/spades-r06.svg', id: '6' },
+    { path: '../imgs/cards/spades/spades-r05.svg', id: '5' },
+    { path: '../imgs/cards/spades/spades-r04.svg', id: '4' },
+    { path: '../imgs/cards/spades/spades-r03.svg', id: '3' },
+    { path: '../imgs/cards/spades/spades-r02.svg', id: '2' }];
 const CARD_BACK = '../imgs/cards/backs/red.svg';
 const SHUFFLED_CARDS = CARDS.sort(() => 0.5 - Math.random());
 
@@ -96,7 +96,7 @@ function createBoard(boardEl) {
 }
 
 function startTimer() {
-    countdown(2, 0o0);
+    countdown(5, 0o0);
 }
 
 function countdown(minutes, seconds) {
@@ -147,31 +147,32 @@ function handleMove(e) {
     //game has started
     let target = e.target;
     let cardIndex = target.id;
-    let selectedCard = document.getElementsByClassName('card'); 
+    let selectedCard = document.getElementsByClassName('card');
     selectedCard = SHUFFLED_CARDS[cardIndex].path;
     let selectedCardId = SHUFFLED_CARDS[cardIndex].id;
-    
+
     if (firstAndSecondChoice.length < 2) {
         target.src = selectedCard;
-        firstAndSecondChoice.push(selectedCardId); 
+        firstAndSecondChoice.push({card: target, id: selectedCardId});
     }
-    if (firstAndSecondChoice.length === 2 && firstAndSecondChoice[0] !== firstAndSecondChoice[1] ) { 
+    if (firstAndSecondChoice.length === 2 && firstAndSecondChoice[0].id !== firstAndSecondChoice[1].id) {
         setTimeout(() => {
-            const card = document.getElementsByClassName('card'); 
-            card.src = CARD_BACK;
-            firstAndSecondChoice = []; 
-        }, 2000);
+            for (let choice of firstAndSecondChoice) {
+                choice.card.src = CARD_BACK;
+            }
+            firstAndSecondChoice = [];
+        }, 1000);
     }
-    // if (firstAndSecondChoice.length >=2 && firstAndSecondChoice[0] === firstAndSecondChoice[1]) {
-    //     setTimeout(() => {
-    //         firstAndSecondChoice.forEach((img) => {
-    //             img.style.display = 'none';
-    //         });
-    //         firstAndSecondChoice = [];
-    //         winningCondition -= 2;
-    //         checkForWin();
-    //     }, 2000);
-    // }
+    if (firstAndSecondChoice.length >= 2 && firstAndSecondChoice[0].id === firstAndSecondChoice[1].id) {
+        setTimeout(() => {
+            for (let choice of firstAndSecondChoice) {
+                choice.card.style.visibility = 'hidden';
+            }
+            firstAndSecondChoice = [];
+            winningCondition -= 2;
+            checkForWin();
+        }, 1000);
+    }
     render();
 }
 
